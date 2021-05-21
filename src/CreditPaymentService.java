1b8e1e749e20760c86d8b8b16f0725e7b2063c28
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-05-21 13:36:12 +0300

    ver 2.0

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,8 @@
 public class CreditPaymentService {
     public long calculate (int  creditSum, int creditPeriod, double lendingRate){
 
-//        double P = lendingRate / 100 / 12; // Месячная процентная ставка
-//        long creditSumInKopecks = (long) S;
-//        double i = (double) lendingRate;
-//        short creditPeriod = (short) n;
-
-        double monthlyPayment = creditSum * (lendingRate + lendingRate/(Math.pow(1 + lendingRate, creditPeriod) - 1)); // Предположительная формула аннуитета
+        double P = lendingRate / 100 / 12; // Месячная процентная ставка
+        double monthlyPayment = creditSum * (P + P/(Math.pow(1 + P, creditPeriod) - 1)); // Предположительная формула аннуитета
 
         return (long) monthlyPayment;
     }
